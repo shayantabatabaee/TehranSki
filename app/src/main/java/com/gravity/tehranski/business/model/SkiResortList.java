29fcec7556eada93986887a22386bbda8d6f4027
shayantabatabee <shayan.ta69@gmail.com>
2017-05-01 15:33:51 +0430

    Fixing Memory Leakage Issue.

--- app/src/main/java/com/gravity/tehranski/business/model/SkiResortList.java
+++ app/src/main/java/com/gravity/tehranski/business/model/SkiResortList.java
@@ -18,9 +18,7 @@ public class SkiResortList {
         resortNames.add("Tochal");
         resortNames.add("Shemshak");
         resortNames.add("Dizin");
-        resortNames.add("Taos");
         resortNames.add("Darbandsar");
-        resortNames.add("wanlong");
         resortNames.add("Abali");
 
     }
